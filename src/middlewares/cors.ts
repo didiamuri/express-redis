2c39b9d489035682283ac619ad640c20dd3d4783
Didierson Amuri <didierson.idi@vodacom.cd>
2024-08-03 04:58:05 +0100

    fix cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 export default cors({
-    origin: ['https://express-redis.didiamuri.dev', 'http://localhost:3000'],
+    origin: ['https://express-redis.didiamuri.dev'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 })
